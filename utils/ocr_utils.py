843e631acf18c48ffa00636061aa92aea18c29de
squibel <samquibel@icloud.com>
2025-01-31 16:32:27 +0100

    init function

--- utils/ocr_utils.py
+++ utils/ocr_utils.py
@@ -1,1 +1,13 @@
 #reconnaissance sudoku
+
+def prepocessing(img):
+
+def get_contours(prep_img):  
+
+def perspective_transform(prep_img, contours):
+
+def get_cell(binary_img):
+
+def get_digit(cell_img,all_cells,size,debug=False):
+
+def extract_digit(cell_img,cell,size):
