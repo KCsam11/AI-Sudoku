292b94e155671a2b55bc1e68aaaca09dd855e391
squibel <samquibel@icloud.com>
2025-01-31 16:33:01 +0100

    init function

--- utils/ocr_utils.py
+++ utils/ocr_utils.py
@@ -8,6 +8,7 @@ def perspective_transform(prep_img, contours):
 
 def get_cell(binary_img):
 
-def get_digit(cell_img,all_cells,size,debug=False):
+def get_digit(gray_img_perspective,all_cells,size,debug=False):
 
-def extract_digit(cell_img,cell,size):
+def extract_digit(img,cell_cord,size):
+
