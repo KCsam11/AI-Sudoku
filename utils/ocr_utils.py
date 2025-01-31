46d0c00958a7db0ace9093808d585019e485ea26
squibel <samquibel@icloud.com>
2025-01-31 16:48:41 +0100

    init get digit

--- utils/ocr_utils.py
+++ utils/ocr_utils.py
@@ -47,18 +47,24 @@ def perspective_transform(prep_img, contours):
 
 def get_cell(binary_img):
     cells = [] 
-    side_col = img.shape[1] 
-    side_row = img.shape[0]
+    side_col = binary_img.shape[1] 
+    side_row = binary_img.shape[0]
     side_row = side_row / 9
     side_col = side_col / 9
     for j in range(9):
         for i in range(9):
-            p1 = (int(i * side_col), int(j * side_row))  # Coin supérieur gauche (entier)
-            p2 = (int((i+1) * side_col), int((j+1) * side_row))  # Coin inférieur droit (entier)
+            p1 = (int(i * side_col), int(j * side_row)) 
+            p2 = (int((i+1) * side_col), int((j+1) * side_row))
             cells.append((p1, p2)) 
 
     return cells
+
 def get_digit(gray_img_perspective,all_cells,size,debug=False):
+    digits = []
+    for cell in all_cells:
+        digit = extract_digit(gray_img_perspective  , cell, size)
+        digits.append(digit)
+    return digits
 
 def extract_digit(img,cell_cord,size):
 
